77d7859d0762a03e2cdb9d8bd652ffdba41a7c89
BradHacker <harkerbd@gmail.com>
2021-03-28 17:10:48 -0400

    Make her user friendly

--- main.go
+++ main.go
@@ -2,13 +2,27 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"math/rand"
 	"net/http"
+	"os"
+	"os/signal"
+	"strconv"
+	"strings"
+	"syscall"
 	"time"
 )
 
+type Stats struct {
+	currentWorkers int
+	urls []string
+	successes int
+	failures int
+	startTime time.Time
+	endTime time.Time
+} 
+
 const (
-	MAX_CONCURRENT_WORKERS = 10
 	SLUG_SYMBOLS = "abcdefghijklmnopqrstuvwxyz0123456789"
 )
 
@@ -21,23 +35,82 @@ func generateSlug(length int) string {
 	return slug
 }
 
-func testUrl(currentRequests *int) {
-	*currentRequests++
+func testUrl(stats *Stats) {
+	(*stats).currentWorkers++
 	url := "https://termbin.com/" + generateSlug(4)
+	fmt.Printf("  | %s\r", url)
 	resp, _ := http.Get(url)
 	if resp.StatusCode >= 200 && resp.StatusCode < 300 {
-		fmt.Printf("Valid URL | %s\n", url)
+		(*stats).successes++
+		(*stats).urls = append((*stats).urls, url)
+	} else {
+		(*stats).failures++
 	}
-	*currentRequests--
+	(*stats).currentWorkers--
+}
+
+func writeOutput(urls []string, outFile string) {
+	println("Saving url's to " + outFile)
+	data := []byte(strings.Join(urls, "\n"))
+	err := ioutil.WriteFile(outFile, data, 0644)
+	if err != nil {
+		println(fmt.Errorf("error writing to output: %v", err))
+		os.Exit(1)
+	}
+}
+
+func printStats(stats Stats) {
+	println("Stats")
+	println("-----")
+	fmt.Printf("Valid Urls:\t%d\n", stats.successes)
+	fmt.Printf("Invalid Urls:\t%d\n", stats.failures)
+	println("-----")
+	fmt.Printf("Total Time:\t\t%s\n", time.Time.Sub(stats.endTime, stats.startTime).String())
+	fmt.Printf("Avg Request Time:\t%0.2fs\n", time.Time.Sub(stats.endTime, stats.startTime).Seconds() / (float64(stats.successes + stats.failures)))
 }
 
 func main() {
-	println("ReTermbin v0.1")
-	currentWorkers := 0
+	if len(os.Args) < 2 {
+		println("Usage: retermbin [max_workers] (output_file)")
+		os.Exit(1)
+	}
 
-	for {
-		if currentWorkers < MAX_CONCURRENT_WORKERS {
-			go testUrl(&currentWorkers)
+	MAX_CONCURRENT_WORKERS, err := strconv.Atoi(os.Args[1])
+	if err != nil {
+		println("Max workers needs to be an integer")
+		os.Exit(1)
+	}
+	OUTPUT_FILE := "output/retermbin_out_" + fmt.Sprint(time.Now().UnixNano()) + ".txt"
+	if len(os.Args) > 2 {
+		OUTPUT_FILE = os.Args[2]
+	}
+
+	println("ReTermbin v0.1")
+	println("^C to stop testing")
+	stats := Stats{
+		currentWorkers: 0,
+		successes: 0,
+		failures: 0,
+		startTime: time.Now(),
+		urls: []string{},
+	}
+
+	sigs := make(chan os.Signal, 1)
+	done := false
+
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		<-sigs
+		stats.endTime = time.Now()
+		writeOutput(stats.urls, OUTPUT_FILE)
+		printStats(stats)
+		done = true
+	}()
+
+	for !done {
+		if stats.currentWorkers < MAX_CONCURRENT_WORKERS {
+			go testUrl(&stats)
 		}
 	}
 }
